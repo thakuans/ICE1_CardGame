83b72a8ee3301fdcbb36f44c8e57858dcfa80975
thakuans <146024870+thakuans@users.noreply.github.com>
2023-09-26 12:53:06 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -8,7 +8,8 @@ package card;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * @author anshika thakur
+ student number 991698396
  */
 public class CardTrick {
     
@@ -19,14 +20,24 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13+1));
+            c.setSuit((Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i] = c;
+             Card luckyCard = new Card();
+            luckyCard.setValue(3);
+            luckyCard.setSuit("Spades");
+
+            boolean found = false;
+            for(int i=0; i<magicHand.length;i++){
+                found = true;
+                break;
+            }
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+        if (found) {
+            System.out.println("congratulation .yougot lucky card");
+        }else{
+            System.out.println("Sorry! try another time .");
+        }
     }
     
 }
